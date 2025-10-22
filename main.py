4e99cd82fc60d0f79a3963de1cde9ea1bbfcc65c
ricaym <aymerincchassagne@gmail.com>
2025-10-22 10:55:35 +0200

    Aymeric : commit 1

--- main.py
+++ main.py
@@ -1,30 +1,21 @@
 from fastapi import FastAPI, Query
 from datetime import datetime
 from weather_service import WeatherService
-from air_service import AirQualityService
 
 app = FastAPI(title="Weather Activity Recommender")
 
-weather_service = WeatherService(api_key="TA_CLE_API_OPENWEATHERMAP")
-air_service = AirQualityService(api_key="TA_CLE_API_OPENAQ")
+weather_service = WeatherService(api_key="23654828e88ada7b918155c3320f3517")
 
 @app.get("/dashboard")
 def get_dashboard(city: str = Query(...), date: str = Query(...)):
     """
     Retourne les données météo + qualité de l'air pour une ville et une date.
     """
-    # Convertir la date
     date_obj = datetime.strptime(date, "%Y-%m-%d")
-
-    # Appel API météo
     weather_data = weather_service.get_weather_forecast(city, date_obj)
     
-    # Appel API qualité de l'air
-    air_data = air_service.get_air_quality(city)
-
     return {
         "ville": city,
         "date": date,
         "meteo": weather_data,
-        "qualite_air": air_data
     }
