cb9d3b8bf1214b1054f56d3e2def4186928738a7
Super Abel <superabel000@gmail.com>
2025-10-22 14:41:14 +0100

    Ajout de la route user

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ load_dotenv()
 from fastapi import FastAPI
 from app.models import models
 from app.core import database
-from app.routers import activity_router, weather_router
+from app.routers import activity_router, weather_router, user_router
 
 models.Base.metadata.create_all(bind=database.engine)
 
@@ -13,6 +13,7 @@ app = FastAPI(title="Météo Activités API")
 
 app.include_router(weather_router.router)
 app.include_router(activity_router.router)
+app.include_router(user_router.router)
 
 @app.get("/")
 def root():
