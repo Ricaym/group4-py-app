b129d22d27d72c219f68ed19bba0b8e8223fac83
Super Abel <superabel000@gmail.com>
2025-10-22 15:03:15 +0100

    importation des classes de modèles dans init

---
+++ app/models/__init__.py
@@ -0,0 +1,9 @@
+# Les classes de modèles
+
+from ._base import Base
+from .enums import IndoorOutdoor, UserRole
+from .user import User
+from .profile import Profile
+from .activity import Activity
+from .activity_instance import ActivityInstance
+from .vote import Vote
