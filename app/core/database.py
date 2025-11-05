32900fdee71cdda6909c0233d6685b7355d7b260
Super Abel <superabel000@gmail.com>
2025-11-05 18:02:26 +0100

    add dynamic url database

--- app/core/database.py
+++ app/core/database.py
@@ -1,9 +1,8 @@
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from app.core.config import settings
 
-DATABASE_URL = "sqlite:///./meteo_activites.db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(settings.database_url, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
 
