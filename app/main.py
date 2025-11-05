d7af7082c5a81d5e171e32fda58212b56fa3ad7b
Super Abel <superabel000@gmail.com>
2025-11-05 18:09:32 +0100

    add vote_router, recommendation_router

--- app/main.py
+++ app/main.py
@@ -5,16 +5,18 @@ load_dotenv()
 from fastapi import FastAPI
 from app.models._base import Base
 from app.core import database
-from app.routers import activity_router, weather_router, user_router, dashboard_router
+from app.routers import activity_router, weather_router, user_router, dashboard_router, vote_router, recommendation_router
 
 Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI(title="Météo Activités API")
 
-app.include_router(weather_router.router)
-app.include_router(activity_router.router)
 app.include_router(user_router.router)
 app.include_router(dashboard_router.router)
+app.include_router(weather_router.router)
+app.include_router(activity_router.router)
+app.include_router(vote_router.router)
+app.include_router(recommendation_router.router)
 
 @app.get("/")
 def root():
